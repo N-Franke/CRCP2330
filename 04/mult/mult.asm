687e491b3439db104fd740816bdb04b34af9bf3c
N-Franke <nfranke@smu.edu>
2018-10-18 21:47:03 -0500

    Started mult.

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,30 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@i //counter
+M=0
+
+@R1 //given input
+D=M
+
+(LOOP)
+@i
+D=M //D=i
+
+@R1
+D= R1-i
+@END 
+D;JGT //if R1>i goto END
+
+@R0  //given new input
+D=D+M //D= R1 + R0
+
+@i
+M=M+1 //increase counter
+@LOOP
+0;JMP //goto loop
+
+(END)
+@END
+0;JMP
